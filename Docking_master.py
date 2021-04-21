b46bf0349813c39397851cfa6d5ebb26e2c1ecac
Jure Fabjan <jure.fabjan93@gmail.com>
2021-04-21 15:51:55 +0200

    Paralelization improvements

--- Docking_master.py
+++ Docking_master.py
@@ -3,51 +3,54 @@ from subprocess import Popen, PIPE
 from pathlib import Path
 from io import TextIOWrapper
 
-settings = [{"root": r"Z:\Ongoing work\Projects\a6 non-review review\Docking\Runs\a1b3\6HUP",
+settings = [{"root": r"Z:\Ongoing work\Projects\a6 non-review review\Docking\Runs\Replication experiment",
              "ligand_path": r"Z:\Ongoing work\Projects\a6 non-review review\Docking\Templates\Ligands",
              "configuration": "gold_template.conf",
              "template_protein": "Template_protein.mol2",
-             "template_ligand": "Template_ligand.mol2"},
-             
-            {"root": r"Z:\Ongoing work\Projects\a6 non-review review\Docking\Runs\a6b3",
-             "ligand_path": r"Z:\Ongoing work\Projects\a6 non-review review\Docking\Templates\Ligands",
-             "configuration": "gold_template.conf",
-             "template_protein": "Template_protein.mol2",
-             "template_ligand": "Template_ligand.mol2"}]
+             "template_ligand": "Template_ligand.mol2"}
+             ]
 
 # Global settings
-process_limit = 5
-n_poses = 10
+process_limit = 10
+n_poses = 200
 processes = []
 processes_ligands = []
+repetitions = 100
 
-ligands = ["XHEII087C.mol2"]
+ligands = ["PZII029.mol2"]
 
 for current_settings in settings:
-    #ligands = [x for x in os.listdir(current_settings["ligand_path"]) if x.endswith("mol2")]
+    # ligands = [x for x in os.listdir(current_settings["ligand_path"]) if x.endswith("mol2")]
     root = Path(current_settings["root"])
     ligand_path = Path(current_settings["ligand_path"])
+    output_dirs = []
 
     for ligand in ligands:
-        if not os.path.exists(Path(root, ligand.split(".")[0])):
-            processes_ligands.append(ligand)
-            processes.append(Popen(["python", "Docking_script_1.py",
-                                    str(root.absolute()),
-                                    current_settings["configuration"],
-                                    current_settings["template_protein"],
-                                    current_settings["template_ligand"],
-                                    str(ligand_path.absolute()),
-                                    ligand], shell=True, stdout=PIPE))
-            time.sleep(10)
-            if len(processes) >= 1:
-                print(f"Current number of processes: {len(processes_ligands)}")
-                if len(processes) >= process_limit:
-                    while True:
-                        stdout = TextIOWrapper(processes[0].stdout, encoding="utf-8").read()
-                        if "Done" in stdout:
-                            processes.pop(0)
-                            break
-                else:
-                    while len([x for x in os.listdir(Path(root, processes_ligands[0].split(".")[0])) if x.startswith("gold_soln")]) < max((n_poses/(len(processes)*process_limit), 5)):
-                        time.sleep(60)
- 
+        for repetition in range(repetitions):
+            out_dir = "_".join((ligand.split(".")[0], str(repetition)))
+            if not os.path.exists(Path(root, out_dir)):
+                processes_ligands.append(ligand)
+                output_dirs.append(out_dir)
+                processes.append(Popen(["python", "Docking_script_1.py",
+                                        str(root.absolute()),
+                                        current_settings["configuration"],
+                                        current_settings["template_protein"],
+                                        current_settings["template_ligand"],
+                                        str(ligand_path.absolute()),
+                                        ligand,
+                                        out_dir], shell=True, stdout=PIPE))
+                time.sleep(10)
+                if len(processes) >= 1:
+                    print(f"Current number of processes: {len(processes_ligands)}\n" + "Current ligands: " + "\n".join(processes_ligands))
+                    if len(processes) >= process_limit:
+                        while True:
+                            stdout = TextIOWrapper(processes[0].stdout, encoding="utf-8").read()
+                            if "Done" in stdout:
+                                processes.pop(0)
+                                print(f"{processes_ligands.pop(0)} run finished")
+                                output_dirs.pop(0)
+                                break
+                    else:
+                        while len([x for x in os.listdir(Path(root, output_dirs[0])) if x.startswith("gold_soln")]) < max((n_poses/process_limit*len(processes), 5)):
+                            time.sleep(60)
+    
--- Docking_script_1.py
+++ Docking_script_1.py
@@ -8,10 +8,11 @@ from pathlib import Path
 #   3. Template ligand file
 #   4. Path to ligands
 #   5. Name of the ligand, which should be run
+#   6. Output directory
 
 arguments = sys.argv[1:]
 
-root, configuration, template_protein, template_ligand, ligand_path, ligand, *_ = arguments
+root, configuration, template_protein, template_ligand, ligand_path, ligand, out_dir, *_ = arguments
 ligand_path = Path(ligand_path)
 os.chdir(root)
 
@@ -20,7 +21,7 @@ run = Docking.Dock(template_protein,
                     template_ligand=template_ligand,
                     fitness_fun="chemscore", rescore_fun="asp",
                     site_radius=7,
-                    output_dir=str(Path(root, ligand.split(".")[0])),
+                    output_dir=str(Path(root, out_dir).absolute()),
                     ndocks=200, autoscale=200, early_termination=False,
                     configuration=configuration, overwrite_protein=False)
 results = Docking.Results(run.settings.conf_file)
