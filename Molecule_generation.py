578cc9c61591b168299c73c6f13de7fbf8415eab
Jure Fabjan <jure.fabjan93@gmail.com>
2021-05-07 13:23:21 +0200

    Adding a systematic molecule builder for MOE

--- Molecule_generation.py
+++ Molecule_generation.py
@@ -1,43 +1,40 @@
 from pathlib import Path
 import os
 from subprocess import run
+import re
+from collections import defaultdict
 
 script = Path(r"C:\Users\Jure\Documents\GitHub\Docking\Docking\Molecule_builder.svl")
 
-def adjust_script(substitutions, template, directory):
-    with open(script, "r") as script_file_handle:
-        script_file_text = script_file_handle.read().split("\n")
-    if substitutions:
-        line_parts = script_file_text[151].split("=")
-        line_parts[1] = " [\'" + "\', \'".join(substitutions).replace("\"", "\'") + "\'];"
-        script_file_text[151] = "=".join(line_parts)
-    if template:
-        line_parts = script_file_text[153].split("\'")
-        line_parts[1] = template
-        script_file_text[153] = "\'".join(line_parts)
-    if directory:
-        line_parts = script_file_text[152].split("\'")
-        line_parts[1] = str(directory.absolute()).replace(os.sep, "/")
-        script_file_text[152] = "\'".join(line_parts)
-    with open(script, "w") as script_file_handle:
-        script_file_handle.write("\n".join(script_file_text))
+def construct_list(tag, l):
+    result = [tag] * (len(l)*2)
+    result[1::2] = l
+    return result
+
 
 if __name__ == "__main__":
     folder = Path(r"C:\Users\Jure\Desktop\Test")
-    total_substitutions = 3
-    all = set([x for x in os.listdir(folder) if x.endswith(".mol2")])
-    organized = [all]
+    total_substitutions = 2
 
-    substitutions = ["H25", "H26", "H27"]
+    substitutions = ["H25", "H26", "H27", "H30", "H19", "H20", "H1"]
+    sidechains = {'F', 'Cl', 'Br', 'Me', 'NH', 'Et', 'Pr', 'iPr', 'tBu', 'CCH', 'CF', 'COMe', 'OMe', 'CN', 'COOH', 'COOMe', 'COOEt', 'NO'}
 
-    # First line
-    adjust_script(substitutions, list(all)[0], folder)
-    run(["moebatch", "-run", str(script.absolute())], shell=True)
+    prohibited = defaultdict(set, {"H25": {"F",}}) # To be filled
 
-    for i in range(total_substitutions-1):
+    for i in range(total_substitutions):
         all = set([x for x in os.listdir(folder) if x.endswith(".mol2")])
-        organized.append(all - set([y for x in organized for y in x]))
-
-        for x in organized[-1]:
-            adjust_script([s for s in substitutions if s[1:] not in x], x, folder)
-            run(["moebatch", "-run", str(script.absolute())], shell=True)
+        if i == 0:
+            organized = [all]
+        else:
+            organized.append(all - set([y for x in organized for y in x]))
+        for ligand in organized[-1]:
+            substitutions_current = {s for s in substitutions if not re.findall(f"[A-z]+{s[1:]}[_|\.]", ligand)}
+            for position in substitutions_current:
+                sidechains_current = sidechains - prohibited[position]
+                run(["moebatch",
+                        "-run", str(script.absolute()),
+                        "-i", str(folder.absolute()),
+                        "-t", ligand] +
+                        construct_list("-s", substitutions_current) +
+                        construct_list("-c", sidechains_current), shell=True)
+        
