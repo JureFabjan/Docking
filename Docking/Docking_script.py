4466bb93b60384ed54a08a6783f91eeaa0abf0fe
Jure Fabjan <jure.fabjan93@gmail.com>
2021-07-19 14:43:10 +0200

    Making Docking_script.py import-friendly

--- Docking/Docking_script.py
+++ Docking/Docking_script.py
@@ -2,20 +2,16 @@ from . import Docking
 import os, sys, time
 from pathlib import Path
 
-if __name__ == "__main__":
-    # Arguments are:
-    #   0. Path to execution
-    #   1. Configuration file name (presumably in the execution path)
-    #   2. Protein file
-    #   3. Template ligand file
-    #   4. Path to ligands
-    #   5. Name of the ligand, which should be run
-    #   6. Output directory
-
-    arguments = sys.argv[1:]
-
-    root, configuration, template_protein, template_ligand, ligand_path, ligand, out_dir, *_ = arguments
-    ligand_path = Path(ligand_path)
+def dock(root, configuration, template_protein, template_ligand, ligand_path, ligand, out_dir):
+    """
+    Initializing the docking procedure.
+    :param root: The root directory in which the docking should be done,
+    :param configuration: Path to the configuration file.
+    :param template_protein: Path to the template protein file.
+    :param template_ligand: Path to the template ligand file.
+    :param ligand_path: Path to the ligand.
+    :param out_dir: Path to the output directory.
+    """
     os.chdir(root)
 
     run = Docking.Dock(template_protein,
@@ -30,3 +26,19 @@ if __name__ == "__main__":
     results.save(save_complex=True, clean_complex=True, extract_all_positions=True)
 
     print("Done")
+
+if __name__ == "__main__":
+    # Arguments are:
+    #   0. Path to execution
+    #   1. Configuration file name (presumably in the execution path)
+    #   2. Protein file
+    #   3. Template ligand file
+    #   4. Path to ligands
+    #   5. Name of the ligand, which should be run
+    #   6. Output directory
+
+    arguments = sys.argv[1:]
+
+    root, configuration, template_protein, template_ligand, ligand_path, ligand, out_dir, *_ = arguments
+    ligand_path = Path(ligand_path)
+    dock(root, configuration, template_protein, template_ligand, ligand_path, ligand, out_dir)
